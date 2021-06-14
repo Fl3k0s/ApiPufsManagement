b87f5ce594a91512afb01512414c94f5c3888c10
Oscar <oscardelamo.oda@gmail.com>
2021-06-14 16:29:36 +0200

    Añadir modificaciones de seguridad

--- src/main/java/com/indytek/pufsmanagement/PufsmanagementApplication.java
+++ src/main/java/com/indytek/pufsmanagement/PufsmanagementApplication.java
@@ -1,11 +1,19 @@
 package com.indytek.pufsmanagement;
 
+
+import com.indytek.pufsmanagement.util.JWTAuthorizationFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication
 /*
 Aplicacion principal que inicia el servidor
  */
@@ -15,3 +23,17 @@ public class PufsmanagementApplication {
     }
 
 }
+@EnableWebSecurity
+@Configuration
+class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .addFilterAfter(new JWTAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .anyRequest().authenticated();
+    }
+}
+
